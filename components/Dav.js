b656cfd8e0f268b038ac429938f413dd5dbd7330
davidingles <davidingleslopez@gmail.com>
2023-03-01 11:13:34 +0100

    cambios

--- components/Dav.js
+++ components/Dav.js
@@ -6,50 +6,26 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Dav(props) {
-  const { nodes, materials } = useGLTF("/4D JONATAN.gltf");
+  const { nodes, materials } = useGLTF("/10D ROANO.gltf");
   return (
     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Plano001.geometry}
-        material={materials["Material.001"]}
+        geometry={nodes["TQ_ARD-geom003"].geometry}
+        material={materials["TQ_ARD_front.001"]}
       />
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Plano002.geometry}
-        material={materials["Material.001"]}
+        geometry={nodes["TQ_ARD-geom003_1"].geometry}
+        material={materials["TQ_ARD_back.002"]}
       />
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Plano003.geometry}
-        material={materials["Material.001"]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Texto.geometry}
-        material={materials["Material.001"]}
-        position={[0.01, 0, 0.31]}
-        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Texto001.geometry}
-        material={materials["Material.001"]}
-        position={[-0.15, 0, 0.59]}
-        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Texto002.geometry}
-        material={materials["Material.001"]}
-        position={[0.01, 0.04, 0.01]}
-        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+        geometry={nodes["TQ_ARD-geom003_2"].geometry}
+        material={materials["TQ_ARD_edge.002"]}
       />
       <mesh
         castShadow
@@ -69,26 +45,8 @@ export function Dav(props) {
         geometry={nodes["TQ_ARD-geom_2"].geometry}
         material={materials.TQ_ARD_edge}
       />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes["TQ_ARD-geom001"].geometry}
-        material={materials["TQ_ARD_front.001"]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes["TQ_ARD-geom001_1"].geometry}
-        material={materials["back-dil"]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes["TQ_ARD-geom001_2"].geometry}
-        material={materials["TQ_ARD_edge.001"]}
-      />
     </group>
   );
 }
 
-useGLTF.preload("/4D JONATAN.gltf");
+useGLTF.preload("/10D ROANO.gltf");
